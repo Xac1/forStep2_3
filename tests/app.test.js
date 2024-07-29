b872f5b9611b94b1d1697659c0c1712c76ac6efa
Valeriy Rusnak <xacan11@gmail.com>
2024-07-29 16:47:23 +0300

    new Dockerfile

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-//const server = require('../index.js');
-//const app = request(server);
+const server = require('../index.js');
+const app = request(server);
 
 describe('GET /', () => {
   var server;
